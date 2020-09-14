faaaee28bc4df7ccf37bfba2ab3074defd28f8ec
Quinton Fults <quinton.fults.work@gmail.com>
2020-09-14 13:28:47 -0700

    code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
-import React from 'react';
+import React from 'react'
+import './App.css'
 
-function App() {
+// const styles = {
+//   heading: {
+//     color: 'red'
+//   }
+// }
+
+let num = 5
+
+const App = () => {
   return (
-  );
+    <>
+      {/* <h1 style={styles.heading}>Hello World!</h1> */}
+      <h1 id="heading">Hello World!</h1>
+      <form>
+        <div className="form-group">
+          <label htmlFor="exampleInputEmail1">Email address</label>
+          <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+          <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+        </div>
+        <div className="form-group">
+          <label htmlFor="exampleInputPassword1">Password</label>
+          <input type="password" className="form-control" id="exampleInputPassword1" />
+        </div>
+        <div className="form-group form-check">
+          <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+          <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+        </div>
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
+    </>
+  )
 }
 
-export default App;
+export default App
